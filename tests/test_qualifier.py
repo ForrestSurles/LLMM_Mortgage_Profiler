2c8a00f3c72e988ce0ca2084cbf37d6b8a1fa28e
Forrest Surles <85535986+fintechProgramAcct@users.noreply.github.com>
2021-07-01 11:09:08 -0500

    ok, now the test file should be equipped to handle a file structure that makes some semblance of sense.

--- tests/test_qualifier.py
+++ tests/test_qualifier.py
@@ -3,20 +3,20 @@ from pathlib import Path
 from app import find_qualifying_loans, save_qualifying_loans
 
 #Import fileio
-from qualifier.utils.fileio import *
-#from qualifier.utils.fileio import load_csv, save_csv
+from loc_fun.utils.fileio import *
+#from loc_fun.utils.fileio import load_csv, save_csv
 
 # Import Calculators
-from qualifier.utils.calculators import *
-#from qualifier.utils.calculators import calculate_monthly_debt_ratio, calculate_loan_to_value_ratio
+from loc_fun.utils.calculators import *
+#from loc_fun.utils.calculators import calculate_monthly_debt_ratio, calculate_loan_to_value_ratio
 
 # Import Filters
-#from qualifier.filters import *
+#from loc_fun.filters import *
 
-from qualifier.filters.credit_score import filter_credit_score
-from qualifier.filters.debt_to_income import filter_debt_to_income
-from qualifier.filters.loan_to_value import filter_loan_to_value
-from qualifier.filters.max_loan_size import filter_max_loan_size
+from loc_fun.filters.credit_score import filter_credit_score
+from loc_fun.filters.debt_to_income import filter_debt_to_income
+from loc_fun.filters.loan_to_value import filter_loan_to_value
+from loc_fun.filters.max_loan_size import filter_max_loan_size
 
 
 def test_save_csv():
