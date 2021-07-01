b75ea67a664d9718529d734d4e93781d394f2e7d
Forrest Surles <85535986+fintechProgramAcct@users.noreply.github.com>
2021-07-01 12:24:30 -0500

    wrote and ran/checked first test for testing the filters

--- tests/test_qualifier.py
+++ tests/test_qualifier.py
@@ -1,10 +1,9 @@
-# Import pathlib
+# Import 'Path' from 'pathlib' to create a workable filepath
 from pathlib import Path
 from app import find_qualifying_loans, save_qualifying_loans
 
-#Import fileio
+#Import 'fileio' functions from 'loc_fun.utils' which include 'load_csv' and 'save_csv'
 from loc_fun.utils.fileio import *
-#from loc_fun.utils.fileio import load_csv, save_csv
 
 # Import Calculators
 from loc_fun.utils.calculators import *
@@ -21,9 +20,15 @@ from loc_fun.filters.max_loan_size import filter_max_loan_size
 
 def test_save_csv():
     # Use Path from pathlib to output the test csv to ./data/output/qualifying_loans.csv
-    csvpath = Path('..\data\output\output_data.csv')
-    test_data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
+    csvpath = Path('.\data\output\output_data.csv')
+    test_data = []
+    temp_list = []
+    for i in range(10):
+        for j in range(10):
+            temp_list.append(j)
+            if j == 0:
+                test_data.append(list(temp_list))
+                temp_list.clear()
     save_csv(csvpath, test_data)
     assert csvpath.exists() == True
 
@@ -33,7 +38,7 @@ def test_calculate_monthly_debt_ratio():
 def test_calculate_loan_to_value_ratio():
     assert calculate_loan_to_value_ratio(210000, 250000) == 0.84
 
-'''
+
 def test_filters():
     bank_data = load_csv(Path('data\daily_rate_sheet.csv'))
     current_credit_score = 750
@@ -43,9 +48,15 @@ def test_filters():
     home_value = 250000
 
     monthly_debt_ratio = 0.375
-
     loan_to_value_ratio = 0.84
 
-    # @TODO: Test the new save_csv code!
-    # YOUR CODE HERE!
-'''
+    qualifying_loans = find_qualifying_loans(
+        bank_data, current_credit_score, debt, income, loan, home_value)
+    compare_against = []
+    compare_against.append(['Bank of Big - Premier Option','300000','0.85','0.47','740','3.6'])
+    compare_against.append(['Bank of Fintech - Premier Option','300000','0.9','0.47','740','3.15'])
+    compare_against.append(['Prosper MBS - Premier Option','400000','0.85','0.42','750','3.45'])
+    compare_against.append(['Bank of Big - Starter Plus','300000','0.85','0.39','700','4.35'])
+    compare_against.append(['FHA Fredie Mac - Starter Plus','300000','0.85','0.45','550','4.35'])
+    compare_against.append(['iBank - Starter Plus','300000','0.9','0.4','620','3.9'])
+    assert qualifying_loans == compare_against
